4fcf26423baf048b8c0267f55699539a1e66723f
dade150 <davidecruciata@gmail.com>
2026-05-15 09:23:31 +0200

    update

--- src/app/core/services/maintenance.service.ts
+++ src/app/core/services/maintenance.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class MaintenanceService {
-  private maintenanceMode = true; 
+  private maintenanceMode = false; 
   isMaintenanceMode(): boolean {
     return this.maintenanceMode;
   }
